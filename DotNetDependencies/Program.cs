ade8e6c40772869f5f7f4194cc01637bb1ef924c
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2026-05-17 01:13:43 +0300

    firstCommit

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDates/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0)); // 0 cases
+    Console.WriteLine("case".ToQuantity(1)); // 1 case
+    Console.WriteLine("case".ToQuantity(5)); // 5 cases
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize()); // a day ago
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize()); // 2 hours ago
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize()); // a day
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize()); // 16 days
+}
---
+++ dotnet/MyConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is: " + DateTime.Now);
+int daysUntilNewYear = (new DateTime(DateTime.Now.Year + 1, 1, 1) - DateTime.Now).Days;
+Console.WriteLine("Days until New Year: " + daysUntilNewYear);
